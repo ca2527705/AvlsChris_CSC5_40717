4cc28ba669ce3c363f61d29e1485f008d0600d42
ca2527705 <cavalos15@student.rcc.edu>
2015-01-07 11:30:51 -0800

    Signed-off-by: ca2527705 <cavalos15@student.rcc.edu>

---
+++ Class_Lab/README.md
@@ -0,0 +1,2 @@
+# AvlsChris_CSC5_40717
+Programming Fundamentals Winer 2015
---
+++ Hmwk/HelloWorld -/main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.cpp
+ * Author: Christopher Avalos
+ * Purpose: Test Run
+ * Created on January 7, 2015, 11:13 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    cout<<"Hello World"<<endl;
+
+    return 0;
+}
+
