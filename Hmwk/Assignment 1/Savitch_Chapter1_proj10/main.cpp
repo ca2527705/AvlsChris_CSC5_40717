4c2ad9b0088d44102e24b15ba4fa7aac64a60bf4
ca2527705 <cavalos15@student.rcc.edu>
2015-01-08 08:57:28 -0800

    Signed-off-by: ca2527705 <cavalos15@student.rcc.edu>

---
+++ Hmwk/Assignment 1/Savitch_Chapter1_proj10/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Benjamin
+ * Created on January 8, 2015, 8:43 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    char c; 
+   cin>> c;
+   cout << endl << endl;
+   
+   cout<<" " << c << c << c << c <<endl;
+   cout<< c << c << "  " << c << c <<endl;
+   cout<< c<<endl;
+   cout<< c<<endl;
+   cout<< c<<endl;
+   cout<< c << c << "  " << c << c << endl;
+   cout<<" " << c << c << c << c <<endl;
+
+    return 0;
+}
+
