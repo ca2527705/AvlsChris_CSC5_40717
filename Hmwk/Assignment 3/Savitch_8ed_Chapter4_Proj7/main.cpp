4a3f7ed2d7d9242d4d52b1240d3be884911b9252
cavalos15 <Usatashi@yahoo.com>
2015-01-22 11:29:28 -0800

    Signed-off-by: cavalos15 <Usatashi@yahoo.com>

--- Hmwk/Assignment 3/Savitch_8ed_Chapter4_Proj7/main.cpp
+++ Hmwk/Assignment 3/Savitch_8ed_Chapter4_Proj7/main.cpp
@@ -20,7 +20,8 @@ const float G=6.673e-8f;//Weak Force attraction cm^3/g/sec^2
 float gravity(float mass1, float mass2, float dist);
 
 int main(int argc, char** argv) {
-    float mass1, mass2, dist;
+    float mass1, mass2, dist,force;
+    char answer;
     // float mEarth=5.972e24f//Mass of earth in Kilograms
     // float wtMark==188.of//Weight of mark in lbs
     // float rEarth=6.371e3;//radius of earth in kilometer
@@ -32,23 +33,51 @@ int main(int argc, char** argv) {
     // cout<<"My weight in dynes = "<<dynes<<endl;
     // cout<<"My Weight in lbs = "<<
     
-    
+    cout<<"This program is designed to output the gravitational\n";
+    cout<<"force between two objects.\n";
+    cout<<"Input the mass of the first object\n";
     cin>>mass1;
+    cout<<"Input the mass of the second object\n";
     cin>>mass2;
+    cout<<"Now input the distance between both objects\n";
     cin>>dist;
     
-    gravity(mass1,mass2,dist);
+    force=gravity(mass1,mass2,dist);
    
     
-    cout<<gravity<<endl;
-            
+    cout<<"The gravitional force between the two objects comes out\n";
+    cout<<"to be "<<force<<" dynes"<<endl;
+    cout<<"Would you like to run it again with another set of masses?\n";
+    cout<<"Enter Y for yes and N for no\n";
+    cin>>answer;
+    while (answer=='Y'||answer=='y'){
+            cout<<"This program is designed to output the gravitational\n";
+            cout<<"force between two objects.\n";
+            cout<<"Input the mass of the first object\n";
+            cin>>mass1;
+            cout<<"Input the mass of the second object\n";
+            cin>>mass2;
+            cout<<"Now input the distance between both objects\n";
+            cin>>dist;
+
+            force=gravity(mass1,mass2,dist);
+
+
+            cout<<"The gravitional force between the two objects comes out\n";
+            cout<<"to be "<<force<<" dynes"<<endl;
+            cout<<"Would you like to run it again with another set of masses?\n";
+            cout<<"Enter Y for yes and N for no\n";
+            cin>>answer;
+    
+}
+    cout<<"Goodbye";
 
     return 0;
 }
 
 float gravity(float mass1, float mass2, float dist){
-    float Force;
-    return (Force=G*mass1*mass2/dist/dist);
+   
+    return (G*mass1*mass2/dist/dist);
 
             
     
