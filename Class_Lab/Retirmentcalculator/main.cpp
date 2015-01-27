53f3240b84aee54c9e58ad6fe1f68ace7ac32c99
ca2527705 <Usatashi@yahoo.com>
2015-01-27 11:22:39 -0800

    Signed-off-by: ca2527705 <Usatashi@yahoo.com>

--- Class_Lab/Retirmentcalculator/main.cpp
+++ Class_Lab/Retirmentcalculator/main.cpp
@@ -5,38 +5,48 @@
  * Created on January 27, 2015, 9:11 AM
  */
 //System Libraries
-#include <cstdlib>
 #include <iostream>
-
+#include <iomanip>
+#include <cstdlib>
 using namespace std;
-
 //User Libraries
-
 //Global Constants
-
 //Function Prototypes
+//For overloaded functions return type does not matter
+//only the types associated with parameters in the argument list
+//Pass by reference vs. pass by value does not count
+//when overloading the function name
 float retirement(float,float,float,int);
-
-//Execution begins Here!
-
+void retirement(float &,float,float,float);
+//Execution Begins Here!
 int main(int argc, char** argv) {
-    //declare variables
-    float salary=75000.0f;
-    float invRate=0.05f;
-    float defPrct=0.15f;//Babylonian 10
+    //Declare variables
+    float salary=150000.0f;
+    float invRate=0.05f;//http://california.municipalbonds.com/bonds/recent
+    float defPrct=0.20f;//Babylonian 10
     float ndSave=salary/invRate;
     float retSav=0;
     float nmYears;
-    cout<<"You need to save $"<<ndSave<<endl;
-    cout<<"Year     Savings"<<endl;
+    //Heading
     cout<<fixed<<setprecision(2)<<showpoint;
+    cout<<"You need to save $"<<ndSave<<endl;
+    cout<<"Year Savings"<<endl;
     //Calculate When We can Retire
     for(int year=1;retSav<=ndSave;year++){
-        retSav=retirement(0.0,invRate,defPrct*salary,year);
-        cout<<setw(4)<<year<<" Savings = $"<<retSav<<endl;
+        //for(int year=1;year<=60;year++){
+        //retSav=retirement(0.0,invRate,defPrct*salary,year);
+        retSav=0;
+        retirement(retSav,invRate,defPrct*salary,year*1.0f);
+        cout<<setw(4)<<year<<setw(12)<<retSav<<endl;
     }
     //Exit stage right!
     return 0;
+}
+void retirement(float &p,float i,float d ,float n){
+    for(int year=1;year<=n;year++){
+        p*=(1+i);
+        p+=d;
+    }
 }
 float retirement(float p,float i,float d,int n){
     for(int year=1;year<=n;year++){
